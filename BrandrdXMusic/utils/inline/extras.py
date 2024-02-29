1fd2303b7a832356f212c1d0ba2b483e035cc6e8
Darkie077 <138225338+Darkie077@users.noreply.github.com>
2024-02-29 20:55:40 +0530

    Update extras.py

--- BrandrdXMusic/utils/inline/extras.py
+++ BrandrdXMusic/utils/inline/extras.py
@@ -18,7 +18,7 @@ def close_markup(_):
         [
             [
                  InlineKeyboardButton(
-                text="🥀 ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/BRANDED_WORLD"
+                text="🥀 ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/thesupport3"
             ),
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
