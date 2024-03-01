03e1caed988b613c4eff2a23767e89510704efaf
Darkie077 <138225338+Darkie077@users.noreply.github.com>
2024-03-01 13:01:00 +0530

    Update userbot.py

--- BrandrdXMusic/core/userbot.py
+++ BrandrdXMusic/core/userbot.py
@@ -11,35 +11,35 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
-            name="BrandrdXAss1",
+            name="RenXAss1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
             no_updates=True,
         )
         self.two = Client(
-            name="BrandrdXAss2",
+            name="RenXAss2",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING2),
             no_updates=True,
         )
         self.three = Client(
-            name="BrandrdXAss3",
+            name="RenXAss3",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING3),
             no_updates=True,
         )
         self.four = Client(
-            name="BrandrdXAss4",
+            name="RenXAss4",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING4),
             no_updates=True,
         )
         self.five = Client(
-            name="BrandrdXAss5",
+            name="RenXAss5",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING5),
@@ -51,9 +51,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("BRANDED_WORLD")
-                await self.one.join_chat("BRANDED_PAID_CC")
-                await self.one.join_chat("BRANDRD_BOT")
+                await self.one.join_chat("thesupport3")
+                await self.one.join_chat("Cyber_sellers_TCE")
+                await self.one.join_chat("ren_music_bot")
             except:
                 pass
             assistants.append(1)
@@ -73,9 +73,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("BRANDED_WORLD")
-                await self.one.join_chat("BRANDED_PAID_CC")
-                await self.one.join_chat("BRANDRD_BOT")
+                await self.two.join_chat("thesupport3")
+                await self.one.join_chat("Cyber_sellers_TCE")
+                await self.one.join_chat("ren_music_bot")
             except:
                 pass
             assistants.append(2)
@@ -95,9 +95,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("BRANDED_WORLD")
-                await self.one.join_chat("BRANDED_PAID_CC")
-                await self.one.join_chat("BRANDRD_BOT")
+                await self.three.join_chat("thesupport3")
+                await self.one.join_chat("Cyber_sellers_TCE")
+                await self.one.join_chat("ren_music_bot")
             except:
                 pass
             assistants.append(3)
@@ -117,9 +117,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("BRANDED_WORLD")
-                await self.one.join_chat("BRANDED_PAID_CC")
-                await self.one.join_chat("BRANDRD_BOT")
+                await self.four.join_chat("thesupport3")
+                await self.one.join_chat("Cyber_sellers_TCE")
+                await self.one.join_chat("ren_music_bot")
             except:
                 pass
             assistants.append(4)
@@ -139,9 +139,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("BRANDED_WORLD")
-                await self.one.join_chat("BRANDED_PAID_CC")
-                await self.one.join_chat("BRANDRD_BOT")
+                await self.five.join_chat("thesupport3")
+                await self.one.join_chat("Cyber_sellers_TCE")
+                await self.one.join_chat("ren_music_bot")
             except:
                 pass
             assistants.append(5)
