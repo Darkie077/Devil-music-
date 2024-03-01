e39f806e06e9b2d8a4b5aa70997001756b9f5f9b
Darkie077 <138225338+Darkie077@users.noreply.github.com>
2024-03-01 12:24:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -45,7 +45,7 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Cyber_sellers_TCE")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/thesupport3")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
 
 
 # Get this credentials from https://developer.spotify.com/dashboard
