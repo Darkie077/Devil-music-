3d91385ce2b33a83566d56521663db8a43d5219c
Darkie077 <138225338+Darkie077@users.noreply.github.com>
2024-03-01 12:50:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
-# Get this value from @BRANDRD_ROBOT on Telegram by /id
+# Get this value from @ren_music_bot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 6258877205))
 
 ## Fill these variables if you're deploying on heroku.
