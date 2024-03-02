dd3f0be3f075ded79f3da5c01611f07fd742794c
Darkie077 <138225338+Darkie077@users.noreply.github.com>
2024-03-02 22:39:14 +0530

    Update speedtest.py

--- BrandrdXMusic/plugins/tools/speedtest.py
+++ BrandrdXMusic/plugins/tools/speedtest.py
@@ -4,9 +4,9 @@ import speedtest
 from pyrogram import filters
 from pyrogram.types import Message
 
-from RenXMusic import app
-from RenXMusic.misc import SUDOERS
-from RenXMusic.utils.decorators.language import language
+from BrandrdXMusic import app
+from BrandrdXMusic.misc import SUDOERS
+from BrandrdXMusic.utils.decorators.language import language
 
 
 def testspeed(m, _):
