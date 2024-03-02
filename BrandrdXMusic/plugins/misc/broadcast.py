fc9eb737ee5463b702d62f6f423e46ffd95086c6
Darkie077 <138225338+Darkie077@users.noreply.github.com>
2024-03-02 22:34:26 +0530

    Update broadcast.py

--- BrandrdXMusic/plugins/misc/broadcast.py
+++ BrandrdXMusic/plugins/misc/broadcast.py
@@ -4,17 +4,17 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.errors import FloodWait
 
-from RenXMusic import app
+from BrandrdXMusic import app
 from config import OWNER_ID
-from RenXMusic.utils.database import (
+from BrandrdXMusic.utils.database import (
     get_active_chats,
     get_authuser_names,
     get_client,
     get_served_chats,
     get_served_users,
 )
-from RenXMusic.utils.decorators.language import language
-from RenXMusic.utils.formatters import alpha_to_int
+from BrandrdXMusic.utils.decorators.language import language
+from BrandrdXMusic.utils.formatters import alpha_to_int
 from config import adminlist
 
 IS_BROADCASTING = False
